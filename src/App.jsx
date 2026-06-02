30e3495011ad0a619d78a4ce895658246966db3a
Arpan Khan <150476574+ArpanCodeX@users.noreply.github.com>
2026-06-02 17:06:05 +0530

    bal er css fix kora holo

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import Edu from "./components/Education/Edu";
 
 function App() {
   return (
-    <div className="bg-[050414]">
+    <div className="bg-red-500">
       <div className="absolute inset-0 bg-[linear-gradient(to_right,4f4f4f2e_1px),linear-gradient(to_bottom,4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(elipse_60%_50%_at_50%_0%_000_70%,transparent_100%,transparent_100%)]"></div>
       <div className="relative pt-20">
-        <Navbar />
+        <Navbar  />
         <Skills />
         <Work />
         <Edu />
